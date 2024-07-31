9f6697ccd1ce98d6907676dfc6681a6aa84832b5
chhantea <r.lalnghenchhana@gmail.com>
2024-07-31 17:37:55 +0530

    initial

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Button>This is button</Button>
+    </div>
+  )
+}
+
+export default Home
